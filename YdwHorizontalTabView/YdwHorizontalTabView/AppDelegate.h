cbff031f0d0732b078725220a9d293283f7014ed
ydw <yangdongwei@xiaokatown.com>
2016-08-18 14:10:15 +0800

    创建工程

---
+++ YdwHorizontalTabView/YdwHorizontalTabView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YdwHorizontalTabView
+//
+//  Created by ydw on 16/6/27.
+//  Copyright © 2016年 ydw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YdwHorizontalTabView/YdwHorizontalTabView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YdwHorizontalTabView
+//
+//  Created by ydw on 16/6/27.
+//  Copyright © 2016年 ydw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
